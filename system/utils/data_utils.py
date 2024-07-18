2a4c99256c8d53659a2486fe0aaea1c828f0faa4
Faiz <LizxFaiz@outlook.com>
2024-07-18 15:20:19 +0800

    更新fedproto

--- system/utils/data_utils.py
+++ system/utils/data_utils.py
@@ -60,6 +60,15 @@ def read_client_data(dataset, idx, is_train=True):
         test_data = [(x, y) for x, y in zip(X_test, y_test)]
         return test_data
 
+def  label_flipping(tensor,a,b):
+    tensor_ = tensor.detach().numpy()
+    for i in range(len(tensor_)):
+        if tensor_[i] == a:
+            tensor_[i] = b
+        elif tensor_[i] == b:
+            tensor_[i] = a
+    tensor = torch.tensor(tensor_)
+    return tensor
 
 def read_client_poison_data(dataset, idx, is_train=True):
     if "News" in dataset:
@@ -72,6 +81,7 @@ def read_client_poison_data(dataset, idx, is_train=True):
         X_train = torch.Tensor(train_data['x']).type(torch.float32)
         y_train = torch.Tensor(train_data['y']).type(torch.int64)
         print(y_train)
+        y_train = label_flipping(y_train,1,7)
         #print("poison")
 
         train_data = [(x, y) for x, y in zip(X_train, y_train)]
